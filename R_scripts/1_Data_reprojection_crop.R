4074d4a416cff29358ca13e2d47b0daf4aed2fd5
GVernh <g.vernham@outlook.comgit config --global user.email g.vernham@outlook.com>
2025-09-01 01:06:01 +0100

    Initial commit

---
+++ R_scripts/1_Data_reprojection_crop.R
@@ -0,0 +1,35 @@
+libs <- c("terra")
+
+installed_libs <- libs %in% rownames(
+  installed.packages())
+
+if (any(installed_libs == F)) {
+  install.packages(
+    libs[!installed_libs]
+  )
+}
+
+invisible(lapply(
+  libs,
+  library,
+  character.only = T
+))
+rm(list = ls())
+
+dir.create(file.path("./Data/", "Processed_data"), showWarnings = FALSE)
+dir.create(file.path("./", "R_scripts"), showWarnings = FALSE)
+
+
+hyd_feat <- terra::rast("./Data/Hydrology/Copernicus_river_lake_shapefile/hyd_feat_25.tif")
+coastline <- terra::vect("./Data/Hydrology/Coastline/gb_shp/gb.shp")
+
+# Project coastline into same projection as target data
+coastline_LEA <- terra::project(coastline, hyd_feat)
+
+# Crop data to UK
+x <- terra::crop(hyd_feat, coastline_LEA)
+plot(x)
+
+# Project data to projects targets crs
+Hyd_fea_wgs84 <- terra::project(x, coastline)
+
